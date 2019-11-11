bb49ccaf599ddbb5ea6c7ae667450ee46d298306
levenetsa <levenecalexey@gmail.com>
2019-11-11 17:44:44 +0300

    Updates

---
+++ precache-manifest.0da94121183f84c838b620eb014bd227.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d2a93e73537abf89188eea15191498",
+    "url": "/vasa-front/index.html"
+  },
+  {
+    "revision": "2d033a6f40ba1a32c086",
+    "url": "/vasa-front/static/css/main.956fbf46.chunk.css"
+  },
+  {
+    "revision": "529b5e564d56be946f5b",
+    "url": "/vasa-front/static/js/2.91ac800f.chunk.js"
+  },
+  {
+    "revision": "2d033a6f40ba1a32c086",
+    "url": "/vasa-front/static/js/main.59acdca7.chunk.js"
+  },
+  {
+    "revision": "47b09f8a158c225685a8",
+    "url": "/vasa-front/static/js/runtime-main.882a0e05.js"
+  }
+]);
