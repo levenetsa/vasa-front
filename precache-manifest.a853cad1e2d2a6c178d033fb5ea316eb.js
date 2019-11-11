05e8bc9b3c9d0c6b2a1851c4c6dfd42941756510
levenetsa <levenecalexey@gmail.com>
2019-11-11 23:24:16 +0300

    Updates

--- precache-manifest.a853cad1e2d2a6c178d033fb5ea316eb.js
+++ precache-manifest.a853cad1e2d2a6c178d033fb5ea316eb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d9649ea67cf28e5e302e3c63b4648d5e",
+    "revision": "efca7692cf8b774ec1776a439a3cc646",
     "url": "/vasa-front/index.html"
   },
   {
-    "revision": "5343f6aa5804c75cf5c7",
-    "url": "/vasa-front/static/css/main.9eda469e.chunk.css"
+    "revision": "91c6a6ac2a8e3f1bfb8d",
+    "url": "/vasa-front/static/css/main.956fbf46.chunk.css"
   },
   {
     "revision": "75889bc19dc3398d9d36",
     "url": "/vasa-front/static/js/2.c13f6657.chunk.js"
   },
   {
-    "revision": "5343f6aa5804c75cf5c7",
-    "url": "/vasa-front/static/js/main.11afe4ce.chunk.js"
+    "revision": "91c6a6ac2a8e3f1bfb8d",
+    "url": "/vasa-front/static/js/main.2ce98ff0.chunk.js"
   },
   {
     "revision": "47b09f8a158c225685a8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vasa-front/precache-manifest.7da62e573a710ff1246d08cea41bdf56.js"
+  "/vasa-front/precache-manifest.a853cad1e2d2a6c178d033fb5ea316eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vasa-front/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
